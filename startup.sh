45a061df2adb6ec87f8fe1d50a77ae79dd264b20
未来工程师 <949825330@qq.com>
2023-04-30 08:56:39 +0800

    1

--- startup.sh
+++ startup.sh
@@ -3,5 +3,6 @@
 # Download and build Nacos
 wget https://github.com/alibaba/nacos/releases/download/2.1.1/nacos-server-2.1.1.tar.gz
 tar -xvf nacos-server-2.1.1.tar.gz
-cd nacos/bin
+cd nacos-server-2.1.1
+cd bin
 sh startup.sh -m standalone
